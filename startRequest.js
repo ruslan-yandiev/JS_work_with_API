257c9e944f0d53b647440318a8331e3891cf5192
ruslan <sqwer1986@gmail.com>
2020-07-20 05:27:42 +0300

    create function startRequest.js

---
+++ startRequest.js
@@ -0,0 +1,20 @@
+'use strict'
+
+async function startRequest(owner, repository) {
+    try {
+        let data = await getIssues(`https://api.github.com/repos/${owner}/${repository}/issues`);
+
+        let progress = 0;
+
+        for (let i = 0; i < data.length; ++i) {
+            if (i + 1 == data.length) {
+                progress = 100
+            } else {
+                progress = ((i / data.length) * 100).toFixed(2);
+            }
+            tagging(data, progress, i);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
